f8bc0e7825eceeca2a83b5f59bca6ee6bbf0c298
ReclaimCamden <36649869+ReclaimCamden@users.noreply.github.com>
2019-12-07 10:39:21 -0600

    Added geolocation to get user location (#5)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import data from './components/FoodBanks.json';
 import Button from '@material-ui/core/Button';
 import NavBar from './components/NavBar.js';
 import StartingPoint from './components/StartingPoint.js';
+import { geolocated } from "react-geolocated";
 import './App.css';
 
 
@@ -11,8 +12,12 @@ class App extends Component {
   
   render(){
     const foodBanks = data
-    
-    return (
+    const { isGeolocationAvailable, isGeolocationEnabled, coords } = this.props;
+    return !isGeolocationAvailable ? (
+      <div>Your browser does not support Geolocation</div>
+  ) : !isGeolocationEnabled ? (
+      <div>Geolocation is not enabled</div>
+  ) : coords ? (
        <div className="App">
          
         <h1>Social Services WebApp</h1>
@@ -26,9 +31,40 @@ class App extends Component {
         <p>Here would be the map to show locations and nearest foodbank? </p>
         <br/>
         <StartingPoint />
+        <table>
+                <tbody>
+                    <tr>
+                        <td>latitude</td>
+                        <td>{coords.latitude}</td>
+                    </tr>
+                    <tr>
+                        <td>longitude</td>
+                        <td>{coords.longitude}</td>
+                    </tr>
+                    <tr>
+                        <td>altitude</td>
+                        <td>{coords.altitude}</td>
+                    </tr>
+                    <tr>
+                        <td>heading</td>
+                        <td>{coords.heading}</td>
+                    </tr>
+                    <tr>
+                        <td>speed</td>
+                        <td>{coords.speed}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-    );    
+    ) : (
+      <div>Getting the location data&hellip; </div>
+  );
   }
  
 }  
-export default App;
+export default geolocated({
+  positionOptions: {
+      enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(App);
--- src/components/StartingPoint.js
+++ src/components/StartingPoint.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 const StartingCoordinates = [39.0551638,-94.5881978]
 
+// const coordset = https://www.google.com/maps/@{StartingCoordinates[0]},{StartingCoordinates[1]},15z
 
 class StartingPoint extends Component {
     
@@ -11,8 +12,9 @@ class StartingPoint extends Component {
             <div>
             <a href="www.google.com">test link to google</a>
             <br></br>
-            <a href="https://www.google.com/maps/@{StartingCoordinates[0]},{StartingCoordinates[1]},15z">this test to StartingCoordinates</a>
+            <a href="https://www.google.com/maps/@{StartingCoordinates[0]},{StartingCoordinates[1]},15z"> this test to StartingCoordinates</a>
             <h2>https://www.google.com/maps/@{StartingCoordinates[0]},{StartingCoordinates[1]},15z</h2>
+           
             </div>
         )
 
