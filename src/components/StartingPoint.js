3ecb1cb9aaad6faf4a12e32e543b1bf7b7722e2a
reclaimcamden <c.coulter982@gmail.com>
2019-12-10 21:03:43 -0600

    Added location to print out on app

--- src/components/StartingPoint.js
+++ src/components/StartingPoint.js
@@ -5,14 +5,15 @@ const StartingCoordinates = [39.0551638,-94.5881978]
 // const coordset = https://www.google.com/maps/@{StartingCoordinates[0]},{StartingCoordinates[1]},15z
 
 class StartingPoint extends Component {
-    
     render(){
-        
+        const url = `https://www.google.com/maps/@${StartingCoordinates[0]},${StartingCoordinates[1]},15z`
+        // const otherurl = "https://www.google.com/maps/@" + StartingCoordinates[0] + "," + StartingCoordinates[1] + ",15z"
+    
         return(
             <div>
             <a href="www.google.com">test link to google</a>
             <br></br>
-            <a href="https://www.google.com/maps/@{StartingCoordinates[0]},{StartingCoordinates[1]},15z"> this test to StartingCoordinates</a>
+            <a href={url}> this test to StartingCoordinates</a>
             <h2>https://www.google.com/maps/@{StartingCoordinates[0]},{StartingCoordinates[1]},15z</h2>
            
             </div>
