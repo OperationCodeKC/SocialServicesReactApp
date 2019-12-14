75a4c608b5d8b366f3c291269a6045aea6fa4e84
Justin Rosenberg <justinksu@gmail.com>
2019-12-14 09:53:19 -0600

    Removed console

--- src/components/StartingPoint.js
+++ src/components/StartingPoint.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 
 class StartingPoint extends Component {
     render(){
-        console.log (this.props);
-        //console.log(latitude);
         const url = `https://www.google.com/maps/@${this.props.latitude},${this.props.latitude}`;
+    
         return(
             <div>
             <a href={url} target="_blank" rel="noopener noreferrer">Google Maps</a>
