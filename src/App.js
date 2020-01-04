3bfbe5921794df86d24f76e0786dc7e6ff039e26
reclaimcamden <c.coulter982@gmail.com>
2020-01-04 17:08:02 -0600

    Added images to public and added comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Table from './components/FoodBanks.js';
-import data from './components/FoodBanks.json';
 import Button from '@material-ui/core/Button';
 import NavBar from './components/NavBar.js';
 import StartingPoint from './components/StartingPoint.js';
@@ -11,15 +10,10 @@ import geojson from 'geojson';
 import geoFoodBanks from './components/geoBanks.json';
 import geoShelters from './components/geoShelters.json';
 import geoHealths from './components/geoHealth.json';
-import burger from './components/burger.jpg';
-import shelter from './components/shelter.png';
-import health from './components/health.png';
-import star from './components/star.jpeg';
+
 
 class App extends Component {
-  
   render(){
-    const foodBanks = data
     const { isGeolocationAvailable, isGeolocationEnabled, coords } = this.props;
     const Map = ReactMapboxGl({
       accessToken:
@@ -41,40 +35,42 @@ class App extends Component {
         <h1 className="title">Social Services WebApp</h1>
         <NavBar />
         
-        <Table foodBankData={foodBanks}/>
+        
         <br/>
         <Button variant="contained" color="primary">
           Nearest Food Bank
         </Button>
 
         <div className="map">
-          <Map  className="mapbox" style= {'mapbox://styles/mapbox/streets-v11'}
-                
-                center={[coords.longitude, coords.latitude]}
-                // for testing when outside of KC using line below for center
-                // center = {[-94.6, 39.025]}
-                containerStyle={{
-                height: '80vh',
-                width: '90vw'
-                }}
-            >            
+{/* Map Function showing markers for all locations in json files. */}
+
+          <Map className="mapbox"
+            style={'mapbox://styles/mapbox/streets-v11'}      
+            center={[coords.longitude, coords.latitude]}
+            // for testing when outside of KC using line below for center
+            // center = {[-94.6, 39.025]}
+            containerStyle={{
+            height: '80vh',
+            width: '90vw'
+          }}
+          >            
             
           <ZoomControl/>
           
-            <Marker
-            coordinates={[-94.6359718, 38.919853200000006]}
-            anchor="bottom"
-            >
-            <img src= {star} alt="star" height='40px' width='40px'/>
-            </Marker>
-
+          <Marker
+          coordinates={[-94.6359718, 38.919853200000006]}
+          anchor="bottom"
+          >
+          <img src= "/images/star.jpeg" alt="star" height='40px' width='50px'/>
+          </Marker>
+{/* markers may need their own component and refactored */}
           {geoFood.features.map(foodBankMarkers =>(
                 <Marker
                 key={foodBankMarkers.properties.Name}
                 coordinates={[foodBankMarkers.geometry.coordinates[0],foodBankMarkers.geometry.coordinates[1]]}
                 anchor="bottom"
                 >
-                  <img src= {burger} alt="food" height='30px' width='30px'/>
+                  <img src= "/images/burger.jpg" alt="food" height='30px' width='30px'/>
                 </Marker>
           
           
@@ -88,33 +84,34 @@ class App extends Component {
                 coordinates={[shelterMarkers.geometry.coordinates[0],shelterMarkers.geometry.coordinates[1]]}
                 anchor="bottom"
                 >
-                  <img src= {shelter} alt="shelter" height='30px' width='30px'/>
+                  <img src= "/images/shelter.png" alt="shelter" height='30px' width='30px'/>
                 </Marker>
           
           
               )
             )
           }
-<div className='medicMarkers' id='medicMarkers'>
-          {geoHealth.features.map(healthMarkers =>(
-                <Marker
-                key={healthMarkers.properties.Name}
-                coordinates={[healthMarkers.geometry.coordinates[0],healthMarkers.geometry.coordinates[1]]}
-                anchor="bottom"
-                >
-                  <img src= {health} alt="medical" height='30px' width='30px'/>
-                </Marker>
-          
-          
-              )
-            )
-          }
-</div>
-          
+
+         {geoHealth.features.map(healthMarkers =>(
+            <Marker
+            key={healthMarkers.properties.Name}
+            coordinates={[healthMarkers.geometry.coordinates[0],healthMarkers.geometry.coordinates[1]]}
+            anchor="bottom"
+            >
+              <img src= "/images/health.png" alt="medical" height='30px' width='30px'/>
+            </Marker>
+      
+      
+          )
+        )
+      } 
 
           </Map>
         </div>
-        
+
+        <Table foodBankData={geoFoodBanks}/>
+        <br/>
+
         <div className="footer">
         <p>Current lat/long {coords.latitude},{coords.longitude}</p>
         <StartingPoint latitude={coords.latitude} longitude={coords.longitude} />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,34 +1,30 @@
 import React, { Component } from 'react';
 import '../App.css';
-import shelter from './shelter.png';
-import food from './burger.jpg';
-import medic from './health.png';
 import { Button } from '@material-ui/core';
+
 
 class NavBar extends Component {
     
     render(){
-        const shelterp = shelter;
-        const medicp = medic;
-        const foodp = food;
+   
     return(
         <div className="navBarCards">
   <Button>
   <div className="column">
     <h2>Food Resources</h2>
-    <img src={foodp} alt="shelter" height='100vh' width="100vh"/>
+    <img src="/images/burger.jpg" alt="shelter" height='100vh' width="100vh"/>
   </div>
   </Button>
   <Button>
   <div className="column">
     <h2>Shelters</h2>
-    <img src={shelterp} alt="shelter" height='100vh' width="100vh"/>
+    <img src="/images/shelter.png" alt="shelter" height='100vh' width="100vh"/>
   </div>
   </Button>
-  <Button onClick={console.log('Not Working')}>
+  <Button>
   <div className="column">
     <h2>Medical/Health</h2>
-    <img src={medicp} alt="shelter" height='100vh' width="100vh"/>
+    <img src="/images/health.png" alt="shelter" height='100vh' width="100vh"/>
   </div>
   </Button>
 </div>
