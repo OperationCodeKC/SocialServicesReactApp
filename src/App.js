beb83aa4ff954117f9d986ade42635019d633aa0
reclaimcamden <c.coulter982@gmail.com>
2020-02-22 09:19:43 -0600

    2nd Test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import StartingPoint from './components/StartingPoint.js';
 import { geolocated } from "react-geolocated";
 import './App.css';
 import TestMap from './components/TestMap.js';
-//Testing Push from githubs
+//Testing Push from github
 class App extends Component {
 
     render(){
